66454fd7f40ee152d0b42d18b1bb9464188bd81a
Zheng-Ling Lai <jamesljlster@gmail.com>
2016-12-01 02:32:17 +0800

    Add stable test

---
+++ test/stable_test.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#include <csv_dataproc.h>
+
+#define TEST_CSV_PATH	"./test.csv"
+
+int main(int argc, char* argv[])
+{
+	int iResult;
+	csv_t csv;
+	
+	while(1)
+	{
+		iResult = csv_read(&csv, TEST_CSV_PATH);
+		if(iResult != CSV_NO_ERROR)
+		{
+			printf("csv_read() failed with error: %d\n", iResult);
+			return -1;
+		}
+
+		csv_delete(csv);
+	}
+
+	return 0;
+}
