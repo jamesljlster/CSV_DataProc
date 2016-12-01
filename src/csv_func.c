fc248e773941e2962e65f709d7f767277dc109eb
Zheng-Ling Lai <jamesljlster@gmail.com>
2016-12-01 09:24:39 +0800

    Using pointer as csv_t type

--- src/csv_func.c
+++ src/csv_func.c
@@ -8,10 +8,10 @@
 int csv_get_value(csv_t csv, int row, int col, double* numPtr)
 {
 	// Checking
-	if(row >= csv.rows || col >= csv.cols)
+	if(row >= csv->rows || col >= csv->cols)
 		return CSV_OUT_OF_RANGE;
 	
-	*numPtr = csv.data[row * csv.cols + col];
+	*numPtr = csv->data[row * csv->cols + col];
 
 	return CSV_NO_ERROR;
 
@@ -20,10 +20,10 @@ int csv_get_value(csv_t csv, int row, int col, double* numPtr)
 int csv_set_value(csv_t csv, int row, int col, double num)
 {
 	// Checking
-	if(row >= csv.rows || col >= csv.cols)
+	if(row >= csv->rows || col >= csv->cols)
 		return CSV_OUT_OF_RANGE;
 	
-	csv.data[row * csv.cols + col] = num;
+	csv->data[row * csv->cols + col] = num;
 
 	return CSV_NO_ERROR;
 }
@@ -43,12 +43,12 @@ int csv_write(csv_t csv, const char* filePath)
 	}
 
 	// Write file
-	for(i = 0; i < csv.rows; i++)
+	for(i = 0; i < csv->rows; i++)
 	{
-		for(j = 0; j < csv.cols; j++)
+		for(j = 0; j < csv->cols; j++)
 		{
-			fprintf(fileWrite, "%lf", csv.data[i * csv.cols + j]);
-			if(j == csv.cols - 1)
+			fprintf(fileWrite, "%lf", csv->data[i * csv->cols + j]);
+			if(j == csv->cols - 1)
 			{
 				fprintf(fileWrite, "\n");
 			}
@@ -69,34 +69,57 @@ RET:
 int csv_create(csv_t* csvPtr, int rows, int cols)
 {
 	int retValue = CSV_NO_ERROR;
+	csv_t tmpCsv = NULL;
 	void* allocTmp = NULL;
 
 	log("enter");
+
+	tmpCsv = malloc(sizeof(struct _CSV));
+	if(tmpCsv == NULL)
+	{
+		retValue = CSV_MEM_FAILED;
+		goto RET;
+	}
 
 	allocTmp = calloc(rows * cols, sizeof(double));
 	if(allocTmp == NULL)
 	{
 		retValue = CSV_MEM_FAILED;
+		goto ERR;
 	}
 	else
 	{
-		csvPtr->data = allocTmp;
-		csvPtr->rows = rows;
-		csvPtr->cols = cols;
+		tmpCsv->data = allocTmp;
+		tmpCsv->rows = rows;
+		tmpCsv->cols = cols;
 	}
+
+	*csvPtr = tmpCsv;
 
 	log("exit");
 
+	goto RET;
+
+ERR:
+	if(tmpCsv != NULL)
+		free(tmpCsv);
+
+RET:
 	return retValue;
 }
 
 int csv_delete(csv_t csv)
 {
 	log("enter");
-
-	if(csv.data != NULL)
+	
+	if(csv != NULL)
 	{
-		free(csv.data);
+		if(csv->data != NULL)
+		{
+			free(csv->data);
+		}
+
+		free(csv);
 	}
 
 	log("exit");
@@ -110,13 +133,13 @@ void csv_print(csv_t csv)
 	
 	log("enter");
 
-	for(i = 0; i < csv.rows; i++)
+	for(i = 0; i < csv->rows; i++)
 	{
-		for(j = 0; j < csv.cols; j++)
+		for(j = 0; j < csv->cols; j++)
 		{
-			printf("%lf", csv.data[i * csv.cols + j]);
+			printf("%lf", csv->data[i * csv->cols + j]);
 
-			if(j == csv.cols - 1)
+			if(j == csv->cols - 1)
 			{
 				printf("\n");
 			}
